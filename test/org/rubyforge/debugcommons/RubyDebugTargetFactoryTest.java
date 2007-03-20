6c4880107ef00a851d40a95b3440261fc6a4ca33
Martin Krauskopf <martin.krauskopf@gmail.com>
2007-03-20 18:09:50 +0000

    test bug in the debug-commons classic debugger. Does not work with odd filenames/paths

---
+++ test/org/rubyforge/debugcommons/RubyDebugTargetFactoryTest.java
@@ -0,0 +1,29 @@
+package org.rubyforge.debugcommons;
+
+import org.rubyforge.debugcommons.model.IRubyBreakpoint;
+
+public class RubyDebugTargetFactoryTest extends DebuggerTestBase {
+    
+    public RubyDebugTargetFactoryTest(String testName) {
+        super(testName);
+    }
+    
+    public void testSpaceAndSemicolonsInPath() throws Exception {
+        setDebuggerType(RubyDebuggerProxy.CLASSIC_DEBUGGER);
+        testFile = writeFile("path spaces semi:colon.rb",
+                "b=10",  // 1
+                "b=11"); // 2
+        testFilePath = testFile.getAbsolutePath();
+        final RubyDebuggerProxy proxy = startDebugger();
+        final IRubyBreakpoint[] breakpoints = new IRubyBreakpoint[] {
+            new TestBreakpoint("path spaces semi:colon.rb", 1),
+        };
+        startDebugging(proxy, breakpoints, 1);
+        waitForEvents(proxy, 1, new Runnable() { // finish spawned thread
+            public void run() {
+                proxy.getDebugTarged().getThreadById(1).resume();
+            }
+        });
+    }
+    
+}
