19c1de3134071dc9fba54661195224a3b3dc23a9
Martin Krauskopf <martin.krauskopf@gmail.com>
2007-03-24 10:12:02 +0000

    Reverting changes commited by mistake (future works)

--- test/org/rubyforge/debugcommons/RubyDebuggerFactoryTest.java
+++ test/org/rubyforge/debugcommons/RubyDebuggerFactoryTest.java
@@ -26,22 +26,4 @@ public class RubyDebuggerFactoryTest extends DebuggerTestBase {
         });
     }
     
-    public void testScriptArguments() throws Exception {
-        setDebuggerType(RubyDebuggerProxy.CLASSIC_DEBUGGER);
-        testFile = writeFile("test.rb",
-                "b=10",  // 1
-                "b=11"); // 2
-        testFilePath = testFile.getAbsolutePath();
-        final RubyDebuggerProxy proxy = startDebugger("--name");
-        final IRubyBreakpoint[] breakpoints = new IRubyBreakpoint[] {
-            new TestBreakpoint("path spaces semi:colon.rb", 1),
-        };
-        startDebugging(proxy, breakpoints, 1);
-        waitForEvents(proxy, 1, new Runnable() { // finish spawned thread
-            public void run() {
-                proxy.getDebugTarged().getThreadById(1).resume();
-            }
-        });
-    }
-    
 }
