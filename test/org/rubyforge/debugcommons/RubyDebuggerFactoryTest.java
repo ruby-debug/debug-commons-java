67b88edc8c2be60137675bb055c3974191c4063e
Martin Krauskopf <martin.krauskopf@gmail.com>
2007-04-03 19:02:56 +0000

    - run testSpaceAndSemicolonsInPath for both engines - use full path in breakpoint command

--- test/org/rubyforge/debugcommons/RubyDebuggerFactoryTest.java
+++ test/org/rubyforge/debugcommons/RubyDebuggerFactoryTest.java
@@ -10,14 +10,14 @@ public class RubyDebuggerFactoryTest extends DebuggerTestBase {
     
     public void testSpaceAndSemicolonsInPath() throws Exception {
         for (RubyDebuggerProxy.DebuggerType debuggerType : RubyDebuggerProxy.DebuggerType.values()) {
-            setDebuggerType(RubyDebuggerProxy.CLASSIC_DEBUGGER);
+            setDebuggerType(debuggerType);
             testFile = writeFile("path spaces semi:colon.rb",
                     "b=10",  // 1
                     "b=11"); // 2
             testFilePath = testFile.getAbsolutePath();
             final RubyDebuggerProxy proxy = startDebugger();
             final IRubyBreakpoint[] breakpoints = new IRubyBreakpoint[] {
-                new TestBreakpoint("path spaces semi:colon.rb", 1),
+                new TestBreakpoint(testFilePath, 1),
             };
             startDebugging(proxy, breakpoints, 1);
             waitForEvents(proxy, 1, new Runnable() { // finish spawned thread
