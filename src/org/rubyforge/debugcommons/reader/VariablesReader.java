58ab666adf67e2d48cbc9a42aa70a3655b70a3cf
Martin Krauskopf <martin.krauskopf@gmail.com>
2008-06-23 11:20:35 +0000

    Seg. fault seems to happen from time to time on this place, not only with Ruby 1.8.6-p36. Tweaking exception message.

--- src/org/rubyforge/debugcommons/reader/VariablesReader.java
+++ src/org/rubyforge/debugcommons/reader/VariablesReader.java
@@ -9,13 +9,13 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 public final class VariablesReader extends XmlStreamReader {
-    
+
     private RubyVariableInfo[] variables;
-    
+
     public VariablesReader(XmlPullParser xpp) {
         super(xpp);
     }
-    
+
     private void parse() throws XmlPullParserException, IOException {
         String element = xpp.getName();
         assert element.equals("variables") || element.equals("processingException");
@@ -27,15 +27,16 @@ public final class VariablesReader extends XmlStreamReader {
             assert false : "Unexpected element: " + element;
         }
     }
-    
+
     private void parseVariables() throws XmlPullParserException, IOException {
         List<RubyVariableInfo> variables = new ArrayList<RubyVariableInfo>();
         while (!(nextEvent() == XmlPullParser.END_TAG && "variables".equals(xpp.getName()))) {
-            // Might happen with native Ruby 1.8.6-p36 and earlier due to Seg. Fault
-            // see http://www.netbeans.org/issues/show_bug.cgi?id=127423
+            // Seems to happen on this place from time to time.
             if (xpp.getName() == null) {
                 throw new XmlPullParserException("xpp.getName() returned 'null'. " +
-                        "Segmentation fault? Using Ruby in version '<= 1.8.6-p36'?");
+                        "Segmentation fault. Bug in the Ruby interpreter/VM. " +
+                        "Please provide possibly exact steps to reproduce " +
+                        "and file a bug against Ruby or debug-commons tracker.");
             }
             assert xpp.getName().equals("variable") : xpp.getName() + " encountered";
             String name = getAttributeValue("name");
@@ -55,25 +56,25 @@ public final class VariablesReader extends XmlStreamReader {
         }
         this.variables = variables.toArray(new RubyVariableInfo[variables.size()]);
     }
-    
+
     private void parseProcessingException() throws XmlPullParserException, IOException {
         Util.severe("Processing exception occured." +
                 " exceptionMessage: " + getAttributeValue("message") +
                 ", exceptionType: " + getAttributeValue("type"));
         ensureEndTag("processingException");
     }
-    
+
     public static RubyVariableInfo[] readVariables(final XmlPullParser xpp)
             throws IOException, XmlPullParserException {
         VariablesReader reader = new VariablesReader(xpp);
         reader.parse();
         return reader.variables;
     }
-    
+
     public static void logProcessingException(final XmlPullParser xpp)
             throws IOException, XmlPullParserException {
         VariablesReader reader = new VariablesReader(xpp);
         reader.parse();
     }
-    
+
 }
