a946a0aace7bc5246ae6fa6328dc6665accc5596
Martin Krauskopf <martin.krauskopf@gmail.com>
2008-10-31 08:59:47 +0000

    Flush possible message during threads and frames reading.

--- src/org/rubyforge/debugcommons/reader/FramesReader.java
+++ src/org/rubyforge/debugcommons/reader/FramesReader.java
@@ -8,27 +8,28 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 public final class FramesReader extends XmlStreamReader {
-    
+
     private RubyFrameInfo[] frames;
-    
+
     public FramesReader(XmlPullParser xpp) {
         super(xpp);
     }
-    
+
     private void parse() throws XmlPullParserException, IOException {
-        List<RubyFrameInfo> frames = new ArrayList<RubyFrameInfo>();
+        List<RubyFrameInfo> _frames = new ArrayList<RubyFrameInfo>();
         assert xpp.getName().equals("frames");
         while (!(nextEvent() == XmlPullParser.END_TAG && "frames".equals(xpp.getName()))) {
+            ErrorReader.flushPossibleMessage(xpp);
             assert xpp.getName().equals("frame") : xpp.getName() + " encountered";
             String file = getAttributeValue("file");
             int line = getAttributeIntValue("line");
             int index = getAttributeIntValue("no");
-			frames.add(new RubyFrameInfo(file, line, index)) ;			
+            _frames.add(new RubyFrameInfo(file, line, index));
             ensureEndTag("frame");
         }
-        this.frames = frames.toArray(new RubyFrameInfo[frames.size()]);
+        this.frames = _frames.toArray(new RubyFrameInfo[_frames.size()]);
     }
-    
+
     public static RubyFrameInfo[] readFrames(final XmlPullParser xpp)
             throws IOException, XmlPullParserException {
         FramesReader reader = new FramesReader(xpp);
--- src/org/rubyforge/debugcommons/reader/ThreadInfoReader.java
+++ src/org/rubyforge/debugcommons/reader/ThreadInfoReader.java
@@ -8,27 +8,28 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 public final class ThreadInfoReader extends XmlStreamReader {
-    
+
     private RubyThreadInfo[] threads;
-    
+
     public ThreadInfoReader(XmlPullParser xpp) {
         super(xpp);
     }
-    
+
     private void parse() throws XmlPullParserException, IOException {
-        List<RubyThreadInfo> threads = new ArrayList<RubyThreadInfo>();
+        List<RubyThreadInfo> _threads = new ArrayList<RubyThreadInfo>();
         assert xpp.getName().equals("threads");
         while (!(nextEvent() == XmlPullParser.END_TAG && "threads".equals(xpp.getName()))) {
+            ErrorReader.flushPossibleMessage(xpp);
             assert xpp.getName().equals("thread") : xpp.getName() + " encountered";
             int id = getAttributeIntValue("id");
             String status = getAttributeValue("status");
             RubyThreadInfo info = new RubyThreadInfo(id, status);
-            threads.add(info) ;
+            _threads.add(info);
             ensureEndTag("thread");
         }
-        this.threads = threads.toArray(new RubyThreadInfo[threads.size()]);
+        this.threads = _threads.toArray(new RubyThreadInfo[_threads.size()]);
     }
-    
+
     public static RubyThreadInfo[] readThreads(final XmlPullParser xpp)
             throws IOException, XmlPullParserException {
         ThreadInfoReader reader = new ThreadInfoReader(xpp);
